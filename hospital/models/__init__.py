976abfc6345a1d53c9e082dd4dcd405ab6d86998
Burnaviour <meharmuzafar760@gmail.com>
2023-10-20 12:02:23 +0500

    new versions -1

--- hospital/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import patients
-from . import appointment
-from . import patient_tag
-from . import playground
-from . import res_config_settings
-from . import operation
-
