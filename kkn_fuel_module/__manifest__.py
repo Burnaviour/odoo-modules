f44bed0a34c14980952eea44c303afac9c499324
Burnaviour <meharmuzafar760@gmail.com>
2023-10-23 15:57:15 +0500

    fixed modules error

--- kkn_fuel_module/__manifest__.py
+++ kkn_fuel_module/__manifest__.py
@@ -1,41 +1,31 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "kkn_pop_module",
-
-    'summary': """
+    "name": "kkn_fuel_module",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
+    "description": """
         Long description of module's purpose
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
+    "author": "My Company",
+    "website": "https://www.yourcompany.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'application': True,
-    'sequence': -400,
-    'version': '0.1',
-
+    "category": "Uncategorized",
+    "application": True,
+    "sequence": -400,
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    "depends": ["base"],
     # always loaded
-    'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/add_pop.xml',
-         'views/edit_pop.xml',
-        'views/close_pop.xml',
-        'views/menus.xml',
-       
+    "data": [
+        "security/ir.model.access.csv",
+        "views/views.xml",
+        "views/templates.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
 }
