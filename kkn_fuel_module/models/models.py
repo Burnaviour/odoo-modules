0c81fc9c4882bac98464549c7adeab68e8f2b175
Burnaviour <meharmuzafar760@gmail.com>
2023-10-24 12:10:18 +0500

    no chanegs

--- kkn_fuel_module/models/models.py
+++ kkn_fuel_module/models/models.py
@@ -6,5 +6,4 @@ from odoo import models, fields, api
 class kkn_fuel_module(models.Model):
     _name = 'kkn_fuel_module.kkn_fuel_module'
     _description = 'kkn_fuel_module.kkn_fuel_module'
-
     name = fields.Char()
